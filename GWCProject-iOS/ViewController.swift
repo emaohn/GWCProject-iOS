d9798f49448f7d6a581c5c4746d4769938e2c9e4
Elena <visimaker@gmail.com>
2019-05-21 15:45:18 -0400

    label #1 addition

--- GWCProject-iOS/ViewController.swift
+++ GWCProject-iOS/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBAction func doneButton(_ sender: Any) {
+    }
+    
 }
 
